72bba90d84af4c518d2173d3534172d9bc4642b1
Jinboi <65874606+Jinboi@users.noreply.github.com>
2024-03-10 23:32:14 -0700

    commit #1 - added ReadMe and added basic codes for the project. I've also set the project target finish date

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+public class Contact
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Phone]
+    public string PhoneNumber { get; set; }
+}
+
+public class PhoneBookContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new PhoneBookContext())
+        {
+            // Create
+            var contact = new Contact { Name = "John Doe", Email = "john@example.com", PhoneNumber = "123-456-7890" };
+            context.Contacts.Add(contact);
+            context.SaveChanges();
+
+            // Read
+            Console.WriteLine("Contacts:");
+            foreach (var savedContact in context.Contacts)
+            {
+                Console.WriteLine($"Id: {savedContact.Id}, Name: {savedContact.Name}, Email: {savedContact.Email}, Phone Number: {savedContact.PhoneNumber}");
+            }
+
+            // Update
+            var contactToUpdate = context.Contacts.FirstOrDefault(c => c.Name == "John Doe");
+            if (contactToUpdate != null)
+            {
+                contactToUpdate.PhoneNumber = "987-654-3210";
+                context.SaveChanges();
+            }
+
+            // Delete
+            var contactToDelete = context.Contacts.FirstOrDefault(c => c.Name == "John Doe");
+            if (contactToDelete != null)
+            {
+                context.Contacts.Remove(contactToDelete);
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- PhoneBook/obj/Debug/net8.0/PhoneBook.AssemblyInfo.cs
+++ PhoneBook/obj/Debug/net8.0/PhoneBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6a94429268f185b0920bb4e9e67af81328ecaaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
